5d19194fd05e7d1c31d072a62ecfefc9fd6c2c63
Administrator <1398565099@qq.com>
2017-11-02 14:58:25 +0800

    code update

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice09SetTextScaleXView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice09SetTextScaleXView.java
@@ -3,6 +3,7 @@ package com.hencoder.hencoderpracticedraw3.practice;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -32,7 +33,16 @@ public class Practice09SetTextScaleXView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        paint.setTextScaleX(0.2f);
+        paint.setTextScaleX(1.2f);
         canvas.drawText(text, 50, 100, paint);
+        Rect bounds = new Rect();
+        //参数里，text 是要测量的文字，start 和 end 分别是文字的起始和结束位置，bounds 是存储文字显示范围的对象，方法在测算完成之后会把结果写进 bounds。
+        paint.getTextBounds(text,0,text.length(),bounds);
+        bounds.left += 50+10;
+        bounds.top += 100+10;
+        bounds.right += 50+10;
+        bounds.bottom += 100+10;
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(bounds, paint);
     }
 }
